31b2a167b468ea99ff8ede8e7a7d5679d78a30a4
Evgeniy Zinchenko <JjjackVat@gmail.com>
2023-05-09 21:45:04 +0300

    1

---
+++ src/main/java/me/zinchenko/City.java
@@ -0,0 +1,39 @@
+package me.zinchenko;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String cityName;
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL)
+    private List<Employee> employees;
+
+    public City() {
+    }
+
+    public City(Integer id, String cityName) {
+        this.id = id;
+        this.cityName = cityName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
